9af48e82947713866768f353da690dca7d5989f9
Alejandro <alejandrozuriguel@gmail.com>
2024-01-28 13:14:06 +0100

    Refactor cookie rejection logic

--- Reject.js
+++ Reject.js
@@ -1,32 +1,31 @@
-setTimeout(() => {
-    console.log('Rejecting Cookies Action');
-    const cookieExtensionURL = window.location.href;
 
-    // Función para ocultar el modal de cookies
-    const hideCookieModal = (selector) => {
-        const modalCookies = document.querySelector(selector);
-        if (modalCookies) {
-            const body = document.querySelector('body');
-            body.classList.remove(...body.classList);
-            modalCookies.style.display = 'none';
-            console.log('Modal Cookies');
-        } else {
-            console.log('No hay modal de cookies');
-        }
-    };
+console.log('Rejecting Cookies Action');
+const cookieExtensionURL = window.location.href;
 
-    // Verificar para elmundo.es o elmundo.com
-    if (cookieExtensionURL.includes('elmundo.es') || cookieExtensionURL.includes('elmundo.com')) {
-        hideCookieModal('.popup-disagreed-cookies');
+// Función para ocultar el modal de cookies
+const hideCookieModal = (selector) => {
+    const modalCookies = document.querySelector(selector);
+    if (modalCookies) {
+        const body = document.querySelector('body');
+        body.classList.remove(...body.classList);
+        modalCookies.style.display = 'none';
+        console.log('Modal Cookies');
+    } else {
+        console.log('No hay modal de cookies');
     }
+};
 
-    // Verificar para elperiodico.com, elperiodico.es o xataka.com
-    if (
-        cookieExtensionURL.includes('elperiodico.com') ||
-        cookieExtensionURL.includes('elperiodico.es') ||
-        cookieExtensionURL.includes('xataka.com')
-    ) {
-        hideCookieModal('#didomi-host');
-    }
-}, 500);
+// Verificar para elmundo.es o elmundo.com
+if (cookieExtensionURL.includes('elmundo.es') || cookieExtensionURL.includes('elmundo.com')) {
+    hideCookieModal('.popup-disagreed-cookies');
+}
+
+// Verificar para elperiodico.com, elperiodico.es o xataka.com
+if (
+    cookieExtensionURL.includes('elperiodico.com') ||
+    cookieExtensionURL.includes('elperiodico.es') ||
+    cookieExtensionURL.includes('xataka.com')
+) {
+    hideCookieModal('#didomi-host');
+}
 
